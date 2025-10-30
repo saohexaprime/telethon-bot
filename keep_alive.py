fc23b5cacc5438f11c98940fecec018ee7359523
saohexaprime <sao.hexaprime@gmail.com>
2025-10-30 21:06:33 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,6 +1,6 @@
+import os
 from flask import Flask
 from threading import Thread
-import os
 
 app = Flask('')
 
@@ -9,7 +9,7 @@ def home():
     return "Bot is alive!"
 
 def run():
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 10000))  # default fallback if PORT not set
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
